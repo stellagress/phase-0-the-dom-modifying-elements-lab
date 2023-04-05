da1c7fed3b8cdf7d2f60b7cef8a6ebcd177a2fdf
Stella Mazzillo <stellagress5@gmail.com>
2023-04-04 20:10:35 -0400

    complete creating and inserting dom nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code here!
+
+// 1-
+// const mainId = document.getElementById('main')
+// mainId.remove()
+
+// either way will work 
+
+document.querySelector('main').remove()
+
+// 2-
+
+
+const newHeader = document.createElement('h1')
+
+
+// 3-
+
+
+newHeader.id = 'victory'
+
+
+// 4-
+
+newHeader.textContent = 'Stella is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
